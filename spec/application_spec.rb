2a2a94d3d805e7fdbed712ea31f5238ea29e4654
Moisés Carvalho <moisescarv27@hotmail.com>
2022-07-01 19:20:40 -0300

    Add route to show stats about page visits

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -72,5 +72,47 @@ describe 'URL Shortener' do
         page.must_have_content 'Unable to locate link'
       end
     end
+
+  end
+
+  describe 'page stats' do
+    describe 'when the page is found' do
+      describe 'and has no visits yet' do
+        it 'display a no visits message' do
+          model = Page.create(target_url: 'http://other-test.com')
+  
+          hasher = MiniTest::Mock.new
+          hasher.expect(:decode, [model.id], ['AB'])
+  
+          Hashids.stub(:new, hasher) do
+            visit '/AB/stats'
+          end
+  
+          page.must_have_content 'Target: http://other-test.com'
+          page.must_have_content 'This link has no visits yet'
+        end
+      end
+
+      describe 'and has some stats' do
+        it '' do
+          model = Page.create(target_url: 'http://other-test.com')
+  
+          hasher = MiniTest::Mock.new
+          hasher.expect(:decode, [model.id], ['AB'])
+
+          TrackVisitService.new(model).track
+  
+          Hashids.stub(:new, hasher) do
+            visit '/AB/stats'
+          end
+  
+          page.must_have_content 'Target: http://other-test.com'
+          page.must_have_content 'Visits: 1'
+        end
+      end
+    end
+
+    describe 'when there is not a page associated with the link' do
+    end
   end
 end
